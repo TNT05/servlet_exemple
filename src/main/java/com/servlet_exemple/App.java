e3423fdd0f13dbde4ec9e80882dc6cc6415436e5
Thierno Barry <thierno05@hotmail.com>
2024-07-08 16:45:32 -0400

    servlet  implementation exemple

--- src/main/java/com/servlet_exemple/App.java
+++ src/main/java/com/servlet_exemple/App.java
@@ -1,20 +1,28 @@
 package com.servlet_exemple;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws LifecycleException
     {
         System.out.println( "Hello World!" );
+
+        // Create a Tomcat server instance
         Tomcat tomcat = new Tomcat();
+        
+        // Set the port before starting the server
+        tomcat.setPort(8080);
+
+        // Add the HelloServlet to the context and map it to /hello URL
+        Context context = tomcat.addContext("", null);
+        Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "HelloServlet");
+
+        // Start the server
         tomcat.start();
-        tomcat.getServer().setPort(8080);
-       tomcat.getServer().await();
+        tomcat.getServer().await();
     }
 }
--- src/main/java/com/servlet_exemple/HelloServlet.java
+++ src/main/java/com/servlet_exemple/HelloServlet.java
@@ -1,16 +1,19 @@
 package com.servlet_exemple;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class HelloServlet extends HttpServlet{
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-  public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException{
-
-    System.out.println("in service method");
-
-  }
-
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("in service method");
+        response.setContentType("text/html");
+        response.getWriter().write("<h2><b>Hello, World!</b></h2>");
+    }
 }
